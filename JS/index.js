8963189303ff20fcbd11253aef7790d2fa60a667
Luis-Salinas-SD <paus.impala21@gmail.com>
2021-05-23 22:40:22 -0500

    Actualizacion hasta el metodo editar

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-//Definición de variables
+//&Definición de variables
 const url = 'https://jsonplaceholder.typicode.com/users/'
 const contenedor = document.querySelector('tbody')
 let resultados = ''
@@ -18,14 +18,15 @@ btnCrear.addEventListener('click', () => {
     opcion = 'crear'
 })
 
-//funcion para mostrar los resultados
+//#funcion para mostrar los resultados
 const mostrar = (articulos) => {
+    //console.log(articulos.results);
     articulos.forEach(articulo => {
         resultados += `<tr>
                             <td>${articulo.id}</td>
                             <td>${articulo.name}</td>
-                            <td>${articulo.name}</td>
-                            <td>${articulo.name}</td>
+                            <td>${articulo.username}</td>
+                            <td>${articulo.email}</td>
                             <td class="text-center">
                                 <a class="btnEditar btn btn-primary">Editar</a>
                                 <a class="btnBorrar btn btn-danger">Borrar</a> </td>
@@ -36,42 +37,68 @@ const mostrar = (articulos) => {
 
 }
 
-//Procedimiento Mostrar
+//#Procedimiento Mostrar
 fetch(url)
     .then(response => response.json())
     .then(data => mostrar(data))
     .catch(error => console.log(error))
 
-
+//& Funcion on
 const on = (element, event, selector, handler) => {
-    element.addEventListener(event, async e => {
+    element.addEventListener(event, e => {
         if (e.target.closest(selector)) {
             handler(e)
         }
     })
 }
 
-//Procedimiento Borrar
+//&Procedimiento Borrar
 on(document, 'click', '.btnBorrar', e => {
-    const fila = e.target.parentNode.parentNode
+    const fila = e.target.closest('tr')
     const id = fila.firstElementChild.innerHTML
-    alertify.confirm("This is a confirm dialog.",
-        async function () {
-            try {
-                const resDelete = await fetch(url + id, {
-                    method: 'DELETE',
-                    headers: {
-                        "Content-type": "application/json; charset=utf-8"
+    alertify.confirm("Estas seguro de eliminar este registro?",
+        function () {
+            fetch(url + id, {
+                method: 'DELETE'
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
                     }
+                    throw 'Error en la peticion'
                 })
-                const post = await resDelete.json()
-                console.log(post);
-                //location.reload()
-            } catch (error) {
-                console.warn(error);
-            }
+                .then(data => {
+                    fila.remove()
+                    console.log('Dato borrado: ' + url + id);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            alertify.success('Borrado')
+
         },
         function () {
-            alertify.error('Cancel')
+            alertify.error('Cancelado')
         })
+})
+
+//&Procedimiento Editar
+
+let idForm = 0
+
+on(document, 'click', '.btnEditar', e => {
+    //caputrar la fila
+    const fila = e.target.closest('tr');
+    //Capturar los datos
+    const idForm = fila.children[0].innerHTML,
+        descripcionForm = fila.children[1].innerHTML,
+        precioForm = fila.children[2].innerHTML,
+        stockForm = fila.children[3].innerHTML;
+    //Se envian los datos al input
+    descripcion.value = descripcionForm
+    precio.value = precioForm
+    stock.value = stockForm
+
+    opcion = 'editar'
+    modalArticulo.show()
 })
